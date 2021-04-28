67145f28851fe3369fafce7b57c366aa86f7cb15
tirannosario <glt.rosario@gmail.com>
2021-04-28 16:56:16 +0200

    Ora l'attacco dello Swordman infligge danni

---
+++ Assets/Scripts/Players/SwordmanScript/SwordmanDamage.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordmanDamage : MonoBehaviour
+{
+    //private Animator animator;
+    private Animator animator;
+    public float swordDamage = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setAnimator(Animator fatherAnimator)
+    {
+        animator = fatherAnimator;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy" && animator.GetCurrentAnimatorStateInfo(0).IsName("Attack"))
+        {
+            other.gameObject.GetComponent<EnemyHealth>().TakeDamage(swordDamage);
+        }
+    }
+}
