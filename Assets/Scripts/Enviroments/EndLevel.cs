71411a4769c513f38fffcc9bc0f179c05e268100
tirannosario <glt.rosario@gmail.com>
2021-06-03 19:50:01 +0200

    impletato (forse) il collider per finire il livello

---
+++ Assets/Scripts/Enviroments/EndLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLevel : MonoBehaviour
+{
+    private int countPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countPlayer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "player")
+        {
+            countPlayer++;
+            if (countPlayer == 3)
+            {
+                Debug.Log("Livello Concluso! Congratulazioni!");
+            }
+        }
+    }
+}
