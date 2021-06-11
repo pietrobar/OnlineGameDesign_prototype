1c7f6b0d63654b47d0236fc6abebc92b24a2a220
Piercosimo Dimilito <piercosimo.dimilito@di.unito.it>
2021-06-11 18:03:20 +0200

    bottone back nella scelta personaggio, reset variabili xp dopo la quit

--- Assets/Scripts/UI/ChoseRoom/ChoseRoom.cs
+++ Assets/Scripts/UI/ChoseRoom/ChoseRoom.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChoseRoom : MonoBehaviour
 {
 
+    public GameObject choseCharacterPanel;
+    public GameObject choseRoomPanel;
     
     public void JoinRoomAndStart()
     {
@@ -13,5 +16,11 @@ public class ChoseRoom : MonoBehaviour
         PhotonNetwork.JoinRoom(rn);
         SceneManager.LoadScene("GameScene");
 
+    }
+
+    public void BackToChoseCharacter()
+    {
+        choseRoomPanel.SetActive(false);
+        choseCharacterPanel.SetActive(true);
     }
 }
