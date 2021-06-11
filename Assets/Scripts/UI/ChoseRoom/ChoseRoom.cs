adc3e6ba78f8711ad4ccad374e70db46b9994237
Piercosimo Dimilito <piercosimo.dimilito@di.unito.it>
2021-06-11 11:34:17 +0200

    aggiunta bottone back nella selezione del personaggio

--- Assets/Scripts/UI/ChoseRoom/ChoseRoom.cs
+++ Assets/Scripts/UI/ChoseRoom/ChoseRoom.cs
@@ -2,16 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChoseRoom : MonoBehaviour
 {
+    public GameObject choseCharacterPanel;
+    public GameObject choseRoomPanel;
 
-    
     public void JoinRoomAndStart()
     {
         string rn = RoomListingPref.roomName;
         PhotonNetwork.JoinRoom(rn);
         SceneManager.LoadScene("GameScene");
 
+    }
+
+    public void BackToChoseCharacter()
+    {
+        choseRoomPanel.SetActive(false);
+        choseCharacterPanel.SetActive(true);
     }
 }
